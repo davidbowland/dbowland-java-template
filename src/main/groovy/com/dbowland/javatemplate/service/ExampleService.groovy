998e3de8cb92285b7d1406873cbe9c6fce96673f
David Bowland <dbowland1@gmail.com>
2021-05-20 01:15:00 -0500

    Add structured logging

--- src/main/groovy/com/dbowland/javatemplate/service/ExampleService.groovy
+++ src/main/groovy/com/dbowland/javatemplate/service/ExampleService.groovy
@@ -4,12 +4,14 @@ import com.dbowland.javatemplate.domain.User
 import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Service
 
+import static net.logstash.logback.argument.StructuredArguments.keyValue
+
 @Slf4j
 @Service
 class ExampleService {
 
   User getUser(final String id) {
-    log.info "Getting user with id ${id}"
+    log.info "Getting user", keyValue("id", id)
     return new User("David", "e@ma.il")
   }
 
@@ -20,17 +22,17 @@ class ExampleService {
 
   String createUser(final User user) {
     final String id = "8675309"
-    log.info "Creating user ${user.name}"
+    log.info "Creating user", keyValue("id", id), keyValue("name", user.name)
     return id
   }
 
   boolean updateUser(final User user) {
-    log.info "Updating user ${user.name}"
+    log.info "Updating user", keyValue("id", user.id)
     return isHealthy()
   }
 
   boolean deleteUser(final String id) {
-    log.info "Deleting user with id ${id}"
+    log.info "Deleting user", keyValue("id", id)
     return isHealthy()
   }
 
