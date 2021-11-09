ef03203e283564e14d62372e6c7e90e1b79371f1
David Bowland <dbowland1@gmail.com>
2021-11-09 01:52:21 -0600

    Add cluster to existing joke security group

--- infrastructure/ecs/clusters.ts
+++ infrastructure/ecs/clusters.ts
@@ -1,12 +1,13 @@
 import * as awsx from '@pulumi/awsx'
 
 import { vpcx } from '../ec2/vpcs'
-import { appName, createdBy, createdFor } from '../vars'
+import { appName, createdBy, createdFor, securityGroupId } from '../vars'
 
 // https://www.pulumi.com/docs/reference/pkg/nodejs/pulumi/awsx/ecs/#Cluster
 
 export const ecsCluster = new awsx.ecs.Cluster('cluster', {
   name: appName,
+  securityGroups: [securityGroupId],
   tags: {
     createdBy,
     createdFor,
